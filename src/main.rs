9edfe169f70860736eb505814a896f578e805cb3
Piotr Łosiniecki <losnieckipiotr@gmail.com>
2026-03-15 18:43:16 +0100

    open window with full screen size

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,21 @@ use imgui::*;
 use imgui_wgpu::{Renderer, RendererConfig};
 use imgui_winit_support::WinitPlatform;
 use pollster::block_on;
+use wgpu::rwh::HasDisplayHandle;
 use std::{sync::Arc, time::Instant};
 use winit::{
     application::ApplicationHandler,
-    dpi::LogicalSize,
+    dpi::{LogicalSize, PhysicalSize, Size},
     event::{Event, WindowEvent},
     event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
-    keyboard::{Key, NamedKey},
-    window::Window,
+    keyboard::{Key, KeyCode, NamedKey, PhysicalKey},
+    platform::macos::WindowExtMacOS,
+    window::{Window, WindowAttributes},
 };
+
+use log::{debug, error, info, trace, warn};
+
+use env_logger::Env;
 
 struct ImguiState {
     context: imgui::Context,
@@ -37,122 +43,32 @@ struct App {
     window: Option<AppWindow>,
 }
 
-impl AppWindow {
-    fn setup_gpu(event_loop: &ActiveEventLoop) -> Self {
-        let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
-            backends: wgpu::Backends::PRIMARY,
-            ..Default::default()
-        });
+// impl App {
+//     fn new(width: f64, height: f64) -> Self {
+//         Self {
+//             width,
+//             height,
+//             window: None,
+//         }
+//     }
+// }
 
-        let window = {
-            let version = env!("CARGO_PKG_VERSION");
+fn main() {
+    env_logger::init();
 
-            let size = LogicalSize::new(1280.0, 720.0);
+    let event_loop = EventLoop::new().unwrap();
 
-            let attributes = Window::default_attributes()
-                .with_inner_size(size)
-                .with_title(format!("imgui-wgpu {version}"));
-            Arc::new(event_loop.create_window(attributes).unwrap())
-        };
+    event_loop.set_control_flow(ControlFlow::Poll);
 
-        let size = window.inner_size();
-        let hidpi_factor = window.scale_factor();
-        let surface = instance.create_surface(window.clone()).unwrap();
 
-        let adapter = block_on(instance.request_adapter(&wgpu::RequestAdapterOptions {
-            power_preference: wgpu::PowerPreference::HighPerformance,
-            compatible_surface: Some(&surface),
-            force_fallback_adapter: false,
-        }))
-        .unwrap();
 
-        let (device, queue) =
-            block_on(adapter.request_device(&wgpu::DeviceDescriptor::default())).unwrap();
+//    if let handle = event_loop.owned_display_handle().display_handle().unwrap() {
+//         let size = winit::monitor::MonitorHandle::size(handle);
 
-        // Set up swap chain
-        let surface_desc = wgpu::SurfaceConfiguration {
-            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-            format: wgpu::TextureFormat::Bgra8UnormSrgb,
-            width: size.width,
-            height: size.height,
-            present_mode: wgpu::PresentMode::Fifo,
-            desired_maximum_frame_latency: 2,
-            alpha_mode: wgpu::CompositeAlphaMode::Auto,
-            view_formats: vec![wgpu::TextureFormat::Bgra8Unorm],
-        };
+//         event_loop.run_app(&mut App::new(size.width, size.height)).unwrap();
+//    }
 
-        surface.configure(&device, &surface_desc);
-
-        let imgui = None;
-        Self {
-            device,
-            queue,
-            window,
-            surface_desc,
-            surface,
-            hidpi_factor,
-            imgui,
-        }
-    }
-
-    fn setup_imgui(&mut self) {
-        let mut context = imgui::Context::create();
-        let mut platform = imgui_winit_support::WinitPlatform::new(&mut context);
-        platform.attach_window(
-            context.io_mut(),
-            &self.window,
-            imgui_winit_support::HiDpiMode::Default,
-        );
-        context.set_ini_filename(None);
-
-        let font_size = (13.0 * self.hidpi_factor) as f32;
-        context.io_mut().font_global_scale = (1.0 / self.hidpi_factor) as f32;
-
-        context.fonts().add_font(&[FontSource::DefaultFontData {
-            config: Some(imgui::FontConfig {
-                oversample_h: 1,
-                pixel_snap_h: true,
-                size_pixels: font_size,
-                ..Default::default()
-            }),
-        }]);
-
-        //
-        // Set up dear imgui wgpu renderer
-        //
-        let clear_color = wgpu::Color {
-            r: 0.1,
-            g: 0.2,
-            b: 0.3,
-            a: 1.0,
-        };
-
-        let renderer_config = RendererConfig {
-            texture_format: self.surface_desc.format,
-            ..Default::default()
-        };
-
-        let renderer = Renderer::new(&mut context, &self.device, &self.queue, renderer_config);
-        let last_frame = Instant::now();
-        let last_cursor = None;
-        let demo_open = true;
-
-        self.imgui = Some(ImguiState {
-            context,
-            platform,
-            renderer,
-            clear_color,
-            demo_open,
-            last_frame,
-            last_cursor,
-        })
-    }
-
-    fn new(event_loop: &ActiveEventLoop) -> Self {
-        let mut window = Self::setup_gpu(event_loop);
-        window.setup_imgui();
-        window
-    }
+    event_loop.run_app(&mut App::default()).unwrap();
 }
 
 impl ApplicationHandler for App {
@@ -188,12 +104,28 @@ impl ApplicationHandler for App {
             }
             WindowEvent::CloseRequested => event_loop.exit(),
             WindowEvent::KeyboardInput { event, .. } => {
+                match event.physical_key {
+                    PhysicalKey::Code(KeyCode::KeyF) => {
+                        if event.state.is_pressed() {
+                            // let monitor = window.window.current_monitor().unwrap();
+                            // let size = monitor.size();
+                            // debug!("WIDTH: {}", size.width);
+                            // debug!("HEIGHT: {}", size.height);
+
+                            window
+                                .window
+                                .set_simple_fullscreen(!window.window.simple_fullscreen());
+                        }
+                    }
+                    _ => {}
+                }
                 if let Key::Named(NamedKey::Escape) = event.logical_key {
                     if event.state.is_pressed() {
                         event_loop.exit();
                     }
                 }
             }
+
             WindowEvent::RedrawRequested => {
                 let delta_s = imgui.last_frame.elapsed();
                 let now = Instant::now();
@@ -215,6 +147,11 @@ impl ApplicationHandler for App {
                     .prepare_frame(imgui.context.io_mut(), &window.window)
                     .expect("Failed to prepare frame");
                 let ui = imgui.context.frame();
+
+                // let monitor = window.window.current_monitor().unwrap();
+                // let size = monitor.size();
+                // debug!("WIDTH: {}", size.width);
+                // debug!("HEIGHT: {}", size.height);
 
                 {
                     let window = ui.window("Hello world");
@@ -315,6 +252,7 @@ impl ApplicationHandler for App {
     ) {
         let window = self.window.as_mut().unwrap();
         let imgui = window.imgui.as_mut().unwrap();
+
         imgui.platform.handle_event::<()>(
             imgui.context.io_mut(),
             &window.window,
@@ -334,10 +272,126 @@ impl ApplicationHandler for App {
     }
 }
 
-fn main() {
-    env_logger::init();
 
-    let event_loop = EventLoop::new().unwrap();
-    event_loop.set_control_flow(ControlFlow::Poll);
-    event_loop.run_app(&mut App::default()).unwrap();
+impl AppWindow {
+    fn new(event_loop: &ActiveEventLoop) -> Self {
+        let mut window = Self::setup_gpu(event_loop);
+        window.setup_imgui();
+
+        window
+    }
+
+    fn setup_gpu(event_loop: &ActiveEventLoop) -> Self {
+        let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::PRIMARY,
+            ..Default::default()
+        });
+
+        let size = event_loop.primary_monitor().unwrap().size();
+
+        let window = {
+            let version = env!("CARGO_PKG_VERSION");
+
+            let attributes = Window::default_attributes()
+                .with_inner_size(LogicalSize::new(size.width, size.height))
+                .with_title(format!("KURWAAAAA {version}"));
+            Arc::new(event_loop.create_window(attributes).unwrap())
+        };
+
+        let size = window.inner_size();
+        let hidpi_factor = window.scale_factor();
+        let surface = instance.create_surface(window.clone()).unwrap();
+
+        let adapter = block_on(instance.request_adapter(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::HighPerformance,
+            compatible_surface: Some(&surface),
+            force_fallback_adapter: false,
+        }))
+        .unwrap();
+
+        let (device, queue) =
+            block_on(adapter.request_device(&wgpu::DeviceDescriptor::default())).unwrap();
+
+        // Set up swap chain
+        let surface_desc = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: wgpu::TextureFormat::Bgra8UnormSrgb,
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Fifo,
+            desired_maximum_frame_latency: 2,
+            alpha_mode: wgpu::CompositeAlphaMode::Auto,
+            view_formats: vec![wgpu::TextureFormat::Bgra8Unorm],
+        };
+
+        surface.configure(&device, &surface_desc);
+
+        let imgui = None;
+        Self {
+            device,
+            queue,
+            window,
+            surface_desc,
+            surface,
+            hidpi_factor,
+            imgui,
+        }
+    }
+
+    fn setup_imgui(&mut self) {
+        let mut context = imgui::Context::create();
+        let mut platform = imgui_winit_support::WinitPlatform::new(&mut context);
+
+        // winit::monitor::MonitorHandle::size(&self)
+
+        platform.attach_window(
+            context.io_mut(),
+            &self.window,
+            imgui_winit_support::HiDpiMode::Default,
+        );
+        context.set_ini_filename(None);
+
+        let font_size = (13.0 * self.hidpi_factor) as f32;
+        context.io_mut().font_global_scale = (1.0 / self.hidpi_factor) as f32;
+
+        context.fonts().add_font(&[FontSource::DefaultFontData {
+            config: Some(imgui::FontConfig {
+                oversample_h: 1,
+                pixel_snap_h: true,
+                size_pixels: font_size,
+                ..Default::default()
+            }),
+        }]);
+
+        //
+        // Set up dear imgui wgpu renderer
+        //
+        let clear_color = wgpu::Color {
+            r: 0.1,
+            g: 0.2,
+            b: 0.3,
+            a: 1.0,
+        };
+
+        let renderer_config = RendererConfig {
+            texture_format: self.surface_desc.format,
+            ..Default::default()
+        };
+
+        let renderer = Renderer::new(&mut context, &self.device, &self.queue, renderer_config);
+        let last_frame = Instant::now();
+        let last_cursor = None;
+        let demo_open = true;
+
+        self.imgui = Some(ImguiState {
+            context,
+            platform,
+            renderer,
+            clear_color,
+            demo_open,
+            last_frame,
+            last_cursor,
+        })
+    }
 }
+
